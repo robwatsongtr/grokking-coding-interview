b9900e0d9f006870330677800e87c49e616041ee
robwatsongtr <rwatso@gmail.com>
2023-02-07 11:30:41 -0800

    begin sqrSortedArr.js

--- removeElement.js
+++ removeElement.js
@@ -7,12 +7,14 @@ element, or 'key' instead of a duplicate.
 
 const removeElement = (arr, key) => {
   let nextNonKey = 0 // start at sub 0 instead of 1 in the duplicate example
-  let next = 0
-  while ( next < arr.length) {
-    if( arr[i] !== key ) {
-      arr[nextNonKey] = arr[next]
+  for( let i = 0; i < arr.length; i++) {
+    console.log(arr)
+    if(arr[i] !== key) {
+      arr[nextNonKey] = arr[i] // copy over i to nextNonKey
       nextNonKey++
-    } 
+    }
   }
   return nextNonKey
 }
+
+console.log(removeElement([3, 2, 3, 6, 3, 10, 9, 3], 3));
---
+++ sqrSortedArr.js
@@ -0,0 +1,10 @@
+/*
+Given a sorted array, create a new array containing squares of all the
+numbers of the input array in the sorted order.
+
+The catch is that there can be negative numbers.
+*/
+
+const squareSortedArr = (arr) => {
+
+}
